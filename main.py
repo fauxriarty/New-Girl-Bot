8a5f35fd10f384efedf684475319e56377211bc8
adi <aditya.chheda04@gmail.com>
2023-05-05 16:47:42 +0530

    now i set up a filter for 4 consecutive dialogues to be tweeted, where atleast 2 of the dialogues should have atleast 3 words so as to avoid losing too much context

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tweepy
 import random
+import time
 
 api_key = "cJuF7sWrFwE6PvOSHhtgrqKvD"
 api_key_secret = "5gfSabIlwrScMbiW6MTHQZyRLqegFS8FXYRuXdcxZJeDNyEIuA"
@@ -14,7 +15,7 @@ clientsecret = "AMm2G1ysWP463wh6Mz8LblT-vOq-7IxpChYemf6hiEdA0eoWTC"
 
 api = tweepy.API(authenticator)
 
-
+"""
 with open('dialogues.txt', 'r', encoding='utf-8') as file:
     lines = file.readlines()
 
@@ -30,4 +31,37 @@ dialogue4 = lines[start_index+3].strip()
 tweet = f"-{dialogue1}\n-{dialogue2}\n-{dialogue3}\n-{dialogue4}\n"
 
 # Post the tweet
-client.create_tweet(text=tweet)
+client.create_tweet(text=tweet) """
+
+with open('dialogues.txt', 'r') as f:
+    dialogues = f.readlines()
+
+# Set the number of dialogues to select for each tweet
+num_dialogues = 4
+
+while True:
+    # Choose a random starting point in the dialogues list
+    start_idx = random.randint(0, len(dialogues) - num_dialogues - 1)
+
+    # Extract the selected dialogues
+    selected_dialogues = dialogues[start_idx:start_idx + num_dialogues]
+
+    # Check if at least 2 dialogues have more than 4 words
+    num_long_dialogues = 0
+    for dialogue in selected_dialogues:
+        if len(dialogue.split()) >= 3:
+            num_long_dialogues += 1
+
+    if num_long_dialogues >= 2:
+        # Concatenate the dialogues into a single string
+        dialogue_str = '\n-'.join(selected_dialogues)
+
+        # Tweet the dialogues
+        client.create_tweet(text=dialogue_str)
+
+        # Wait for 24 hours before tweeting again
+        time.sleep(86400)
+
+    else:
+        # Get a new set of dialogues
+        continue
