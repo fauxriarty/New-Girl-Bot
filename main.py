c46ac4e4bab66fe530f71abf8c342ecc172bc497
adi <79994811+fauxriarty@users.noreply.github.com>
2023-05-13 00:07:07 +0530

    removed my access keys so the code is not functional now, just for github viewing

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import random
 import time
 import requests
 
-api_key = "cJuF7sWrFwE6PvOSHhtgrqKvD"
-api_key_secret = "5gfSabIlwrScMbiW6MTHQZyRLqegFS8FXYRuXdcxZJeDNyEIuA"
-access_token = "1653852947812581376-Ye6qQtQ1eldIgLP26ACoCrUxPIZf2i"
-access_token_secret = "IHDPirvT3QFBRpT9U17bDNKIDmCeoS5l2aa354M0jvA1h"
-bearer = r"AAAAAAAAAAAAAAAAAAAAACLenAEAAAAAsLzoCCFBXgwwo1wXcqZ9EkZd4eQ%3DY9VRw25UMETNC8fIa0phlOw9ph9ZCXizsS3RJsrLuqcNLVW9QI"
+api_key = "xyz"
+api_key_secret = "abc"
+access_token = "123"
+access_token_secret = "456"
+bearer = r"abc"
 client = tweepy.Client(bearer, api_key, api_key_secret, access_token, access_token_secret)
 authenticator = tweepy.OAuth1UserHandler(api_key, api_key_secret, access_token, access_token_secret)
 
-clientid = "dHlPTlZ2bnNibjBHNzFvMGlJWHk6MTpjaQ"
-clientsecret = "AMm2G1ysWP463wh6Mz8LblT-vOq-7IxpChYemf6hiEdA0eoWTC"
+clientid = "klmn"
+clientsecret = "abcdef"
 
 api = tweepy.API(authenticator)
 
